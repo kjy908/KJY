4bf28d6f943e1bed1552b47020bb2031f9860f9e
hanbit <hanbit@hb5020>
2016-12-06 14:46:43 +0900

    최초 커밋

---
+++ src/main/java/com/hanbit/animal/Dog.java
@@ -0,0 +1,53 @@
+package com.hanbit.animal;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Dog {
+	
+	public static final String DEFAULT_NAME = "멍멍이";
+	
+	private String name;
+	private final String kind;
+	private String color;
+	
+	public Dog(String kind, String color) {
+		this(DEFAULT_NAME, kind, color);
+	}
+	
+	public Dog(String name, String kind, String color){
+		this.name = name;
+		this.kind = kind;
+		this.color = color;
+	}
+	@Override
+	public String toString() {
+		return name + ":" + kind + ":" + color;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		if(StringUtils.isBlank(name)) {
+			name = DEFAULT_NAME;
+		}
+		
+		this.name = name;
+	}
+	
+	public String getKind() {
+		return kind;
+	}
+	public void setColor(String color) {
+		if(StringUtils.isBlank(color)) {
+			return;
+		}
+	}
+
+	public String getColor() {
+		return color;
+	}
+	
+
+}
